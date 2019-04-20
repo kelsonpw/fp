a4b4136ff7c07f6be388323b07194d5c336e1b70
Kelson Warner <kelson@null.net>
2019-04-20 11:23:51 -0700

    add pointfree

---
+++ pointfree.js
@@ -0,0 +1,9 @@
+// Pointfree programming
+
+const array = [1, 2, 3];
+const double = x => x * 2;
+const doubled = array.map(double);
+console.log(doubled);
+// Legibility
+// Reduce surface area for bugs
+// Unit test named fns
