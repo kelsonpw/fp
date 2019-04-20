2228a38e54a008015878adc3fb6e49b72e11a404
Kelson Warner <kelson@null.net>
2019-04-20 11:34:26 -0700

    add arg order

---
+++ argumentorder.js
@@ -0,0 +1,24 @@
+// Argument order
+
+// const mapa = (arr, cb) => arr.map(cb);
+// const mapb = (cb, arr) => arr.map(cb);
+
+// const map = array => cb => array.map(cb);
+
+// const arr = [1, 2, 3, 4, 5];
+// const double = n => n * 2;
+
+// const withArr = map(arr);
+
+// console.log(withArr(double));
+// console.log(withArr(n => n * 3));
+// console.log(arr.map(n => n * 4));
+
+const map = cb => array => array.map(cb);
+
+const arr = [1, 2, 3, 4, 5];
+const double = n => n * 2;
+
+const withDouble = map(double);
+console.log(withDouble(arr));
+console.log(withDouble(withDouble(arr)));
